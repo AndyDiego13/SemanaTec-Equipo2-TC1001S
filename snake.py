de96483974edf403a4f7ccad7772301914f11c10
ivalani <75494927+ivalani@users.noreply.github.com>
2021-10-31 01:17:08 -0500

    random color library
    For the code to work, the random libraries must be added. The randint () method returns an integer number selected element from the specified range.

--- snake.py
+++ snake.py
@@ -13,6 +13,9 @@ from turtle import *
 from random import randrange
 from freegames import square, vector
 
+from random import randint
+import turtle
+
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
